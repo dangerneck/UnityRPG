123b9dd8abeb593209f50e75045992d22fa4f62b
Andrew Moore <andrewmoore@amac.local>
2014-11-08 15:30:33 +1100

    collision detection improved, gravity introducted, multi-levelled hiding fixed, sloped and stairs walking added

--- Assets/Scripts/CameraHideColliderManager.cs
+++ Assets/Scripts/CameraHideColliderManager.cs
@@ -7,25 +7,34 @@ public class CameraHideColliderManager : MonoBehaviour {
 	BoxCollider collider;
 	public Camera camera;
 	public GameObject player;
-
+	PlayerControl playerControl;
 
 	// Use this for initialization
 	void Start () {
 		collider = GetComponent<BoxCollider> ();
-		collider.size = new Vector3 (5, 10, 10);
+		collider.size = new Vector3 (1, 10, 1);
+		playerControl = player.GetComponent<PlayerControl>();
 	}
 
 	void FixedUpdate () 
 	{
-		collider.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);
+		collider.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + 6, player.transform.position.z);
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
-		var renderers = other.GetComponentsInChildren<Renderer>().ToList ();
-		foreach(var renderer in renderers){
-			renderer.enabled = false;
+	
+		var bounds = other.bounds;
+		if (!bounds.Contains (playerControl.playerBounds[0]) &&
+		    !bounds.Contains (playerControl.playerBounds[1]) &&
+		    !bounds.Contains (playerControl.playerBounds[2]) &&
+		    !bounds.Contains (playerControl.playerBounds[3])){
+			var renderers = other.GetComponentsInChildren<Renderer>().ToList ();
+			foreach(var renderer in renderers){
+				renderer.enabled = false;
+			}
 		}
+
 	}
 
 	void OnTriggerExit(Collider other)
--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -8,10 +8,15 @@ public class PlayerControl : MonoBehaviour {
 	public float cameraZ = 0f;
 	public float cameraX = 3f;
 	public float accelerationPercent = 0.2f;
+
+	public float playerSize = 1.0f;
+	public Vector3[] playerBounds;
+
 	Vector3 moveDirection;
 	float moveSpeed;
 
 	float moveLerper = 0.0f;
+
 
 	public Camera camera;
 	Transform body;
@@ -21,9 +26,12 @@ public class PlayerControl : MonoBehaviour {
 		body = this.transform;
 		camera.transform.position = new Vector3 (body.position.x + cameraX, body.position.y + cameraY, body.position.z + cameraZ);
 		camera.transform.LookAt (body.position);
+		playerBounds = new Vector3[4];
 	}
 
 	void FixedUpdate () {
+
+		// Handle move acceleration through lerp
 		if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0) 
 		{
 			if (moveLerper != 1)
@@ -41,13 +49,33 @@ public class PlayerControl : MonoBehaviour {
 			}
 		}
 
+		// Collision detection for move
 		moveDirection = new Vector3(Mathf.Lerp (0, Input.GetAxis("Horizontal") * maxSpeed, moveLerper), 0, Mathf.Lerp (0, Input.GetAxis("Vertical") * maxSpeed, moveLerper));
 		var moveMagnitude = moveDirection.magnitude;
 		var normDir = moveDirection / moveMagnitude;
-		if (!Physics.Raycast (body.position, normDir, moveMagnitude)) {
+
+		RaycastHit rayHit;
+		if (!Physics.SphereCast (body.position, playerSize/2, normDir, out rayHit, moveMagnitude)) {
+			body.position = body.position + moveDirection;
+		}else if (!Physics.SphereCast (body.position + new Vector3(0f, 0.5f, 0f), playerSize/2, normDir, out rayHit, moveMagnitude)) {
+			body.position = body.position + moveDirection + new Vector3(0f, 0.5f, 0f);
+		}
+	
+		//Gravity collision check & move
+		moveDirection = new Vector3(0f,-0.1f,0f);
+		moveMagnitude = moveDirection.magnitude;
+		if (!Physics.SphereCast (body.position, playerSize/2, Vector3.down, out rayHit, moveMagnitude)) {
 			body.position = body.position + moveDirection;
 		}
 
+		// Update player bounds
+		float halfPlayerSize = playerSize / 2;
+		playerBounds[0] = new Vector3(body.position.x - halfPlayerSize, body.position.y + halfPlayerSize, body.position.z + halfPlayerSize);
+		playerBounds[1] = new Vector3(body.position.x + halfPlayerSize, body.position.y + halfPlayerSize, body.position.z - halfPlayerSize);
+		playerBounds[2] = new Vector3(body.position.x - halfPlayerSize, body.position.y - halfPlayerSize, body.position.z - halfPlayerSize);
+		playerBounds[3] = new Vector3(body.position.x + halfPlayerSize, body.position.y - halfPlayerSize, body.position.z + halfPlayerSize);
+
+		// Move camera to player's new position
 		camera.transform.position = new Vector3 (body.position.x + cameraX, body.position.y + cameraY, body.position.z + cameraZ);
 		camera.transform.LookAt (body.position);
 	}
